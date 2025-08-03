455fd242dbbc728ba50147a806c8d2fa148e6d59
Jaisapthagiri <imjai6504@gmail.com>
2025-08-03 08:21:34 +0530

    completed client side

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -3,11 +3,12 @@ import { dummyStoriesData } from '../assets/assets'
 import { Plus } from 'lucide-react'
 import moment from 'moment'
 import StoryModel from './StoryModel'
+import StoryViewer from './StoryViewer'
 
 
 const StoriesBar = () => {
 
-    const [Stories, setStories] = useState([])
+    const [stories, setStories] = useState([])
     const [showModal, setShowModal] = useState(false)
     const [viewStory, setViewStory] = useState(null)
 
@@ -20,7 +21,7 @@ const StoriesBar = () => {
     }, [])
 
     return (
-        <div className='w-screen no-scrollbar overflow-x-auto px-4 sm:w-[calc(100vh-240px)] lg:max-w-2xl'>
+        <div className='w-screen no-scrollbar overflow-x-auto px-4 sm:w-[calc(100vh-60px)] lg:max-w-2xl'>
             <div className='flex gap-4 pb-5'>
                 <div onClick={()=>setShowModal(true)} className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white'>
                     <div className='h-full flex flex-col items-center justify-center p-4'>
@@ -31,14 +32,14 @@ const StoriesBar = () => {
                     </div>
                 </div>
                 {
-                    Stories.map((story, index) => (
-                        <div key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}>
+                    stories.map((story, index) => (
+                        <div onClick={() => setViewStory(story)} key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}>
                             <img src={story.user.profile_picture} alt="" className='absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow' />
                             <p className='absolute top-16 left-3 text-white/60 text-sm truncate max-w-24'>{story.content}</p>
                             <p className='text-white absolute bottom-1 right-2 z-10 text-xs'>{moment(story.createdAt).fromNow()}</p>
                             {
                                 story.media_type !== "text" &&
-                                <div className='absolute inset-0 z-1 rounded-lg bg-black overflow-hidden'>
+                                <div className='absolute inset-0 z-0 rounded-lg bg-black overflow-hidden'>
                                     {
                                         story.media_type === "image" ?
                                             <img src={story.media_url} alt="" className='h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80' />
@@ -52,6 +53,8 @@ const StoriesBar = () => {
                 }
             </div>
                 {showModal && <StoryModel setShowModal={setShowModal} fetchStories={fetchStories} />}
+
+                {viewStory && <StoryViewer viewStory={viewStory} setViewStory={setViewStory} /> }
         </div>
     )
 }
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
+import PostCard from '../components/PostCard'
+import { assets } from '../assets/assets'
+import RecentMessage from '../components/RecentMessage'
 
 const Feed = () => {
 
@@ -18,23 +21,28 @@ const Feed = () => {
   }, [])
 
   return !loading ? (
-    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start ml-20 xl:gap-8 '>
+    <div className='h-full w- overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start ml-20 xl:gap-8 '>
       <div>
         <StoriesBar />
-        <div className='p-8 space-y-6'>
-          List of Posts
+        <div className='p-4 space-y-6'>
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
         </div>
       </div>
 
-      <div>
-        <div>
-          <h1>Sponsored</h1>
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className=''>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
         </div>
-        <h1>Recent Messages</h1>
+        <RecentMessage />
       </div>
     </div>
   ) :
     <Loading />
 }
 
-export default Feed
+export default Feed;
